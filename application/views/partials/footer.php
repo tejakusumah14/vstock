4745465279a4c27ff3e958022f81c28be7c4fb50
tejakusumah14 <kusumahteja6@gmail.com>
2024-08-01 23:10:04 +0700

    comit satu

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>copyright by © 2024 by <a href="https://duniamu38.com" target="_blank">Teja Kusumah</a></span>
+		</div>
+	</div>
+</footer>
