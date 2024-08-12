18fa1b2f61165facea7b0e4fddce19ad8fa309fd
Isman <ajiisman2@gmail.com>
2024-08-12 07:10:21 +0700

    fix: case sensitive controller

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://vstock.test/';
-$config['base_url'] = 'https://vstock.majalio.com/';
+$config['base_url'] = 'http://vstock.test/';
+// $config['base_url'] = 'https://vstock.majalio.com/';
 
 /*
 |--------------------------------------------------------------------------
